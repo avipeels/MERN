0e2e8a365c45e95b7c416cd3c554d55ec179c668
Avinash Peelukhana <avinash.peelukhana@accenture.com>
2018-03-30 20:36:18 +0530

    mod - reducer

--- src/app.js
+++ src/app.js
@@ -1,33 +1,38 @@
 import { createStore } from "redux";
 
 //step3 - define reducer
-const reducer = function (state = {}, action) {
+const reducer = function (state = [], action) {
   switch (action.type) {
     case 'POST_BOOK':
-      return state = action.payload;
+      let books = state.concat(action.payload);
+      return books;
       break;
   }
   return state;
 }
-
 //step1 - create store
 const store = createStore(reducer);
 
 store.subscribe(() => {
   console.log('current state is:', store.getState());
-  console.log('current state is:', store.getState().price);
+  console.log('current state is:', store.getState()[1].price);
 
 
 })
 //step2 - create action and dispatch them
 store.dispatch({
   type: 'POST_BOOK',
-  payload: {
+  payload: [{
     id: 1,
     title: 'title',
     description: 'desc',
     price: 22.22
-  }
+  }, {
+    id: 2,
+    title: 'title',
+    description: 'desc',
+    price: 33.22
+  }]
 });
 
 
