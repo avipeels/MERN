7c5e81b29ea3574067dd06cfc7ec20d20c17f53a
Avinash Peelukhana <avinash.peelukhana@accenture.com>
2018-05-18 21:46:53 +0530

    upgraded react router to v4

--- src/app.js
+++ src/app.js
@@ -9,7 +9,8 @@ import reducers from "./reducers/index";
 import { addToCart } from "./actions/cartActions";
 import { postBooks, deleteBook, updateBook } from "./actions/booksActions";
 //react router
-import { Route, Router, IndexRoute, browserHistory } from "react-router";
+// import { Router, Route, IndexRoute, browserHistory } from "react-router";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 //step1 - create store
 //step2 - create actions and dispatch them
@@ -23,15 +24,21 @@ import Menu from "./components/Menu";
 import Footer from "./components/Footer";
 import Cart from "./components/Cart";
 import BooksForm from "./components/BooksForm";
-import Main from './Main'
+import Main from './Main';
 const Routes = (
   <Provider store={store}>
-    <Router history={browserHistory}>
-      <Route path="/" component={Main} />
-      <IndexRoute component={BookList} />
-      <Route path="/admin" component={BooksForm} />
-      <Route path="/cart" component={Cart} />
-    </Router>
+    <BrowserRouter>
+      <div>
+        <Menu />
+        <Switch>
+          <Route exact path="/" component={BookList} />
+          <Route path="/admin" component={BooksForm} />
+          <Route path="/cart" component={Cart} />
+        </Switch>
+        <Footer />
+      </div>
+    </BrowserRouter>
+
   </Provider>
 )
 // BOOK ACTIONS
